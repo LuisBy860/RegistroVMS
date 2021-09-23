731d85317e193a79dbcdbc25e56ea4b5a1bf1321
Luis Serrano <84140881+LuisBy860@users.noreply.github.com>
2021-09-23 16:47:19 -0600

    GoFour
    Primer vista

---
+++ RegistroVMS/RegistroVMS/Controllers/PersonaController.cs
@@ -0,0 +1,34 @@
+﻿using RegistroVMS.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistroVMS.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult PersonaViewModel()
+        {
+
+            return View();
+        }
+        public ActionResult PersonaViewModels(PersonaViewModel persona)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View("PersonaViewModel" ,persona);
+            }
+            return Redirect("PersonaViewModel");
+
+        }
+
+    }
+}
--- RegistroVMS/RegistroVMS/Models/ViewModels/PersonaViewModel.cs
+++ RegistroVMS/RegistroVMS/Models/ViewModels/PersonaViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,23 @@ namespace RegistroVMS.Models.ViewModels
 {
     public class PersonaViewModel
     {
+        [Display (Name = "Nombre")]
+        [Required (ErrorMessage = "estos campos son requeridos")]
         public string Nombre { get; set; }
+
+        [Display(Name = "Edad")]
+        [Required(ErrorMessage = "estos campos son requeridos")]
         public int Edad { get; set; }
+
+        [Display(Name = "Sexo")]
+        [Required(ErrorMessage = "estos campos son requeridos")]
+
         public string Sexo { get; set; }
+
+        [Display(Name = "N_Educativo")]
+        [Required(ErrorMessage = "estos campos son requeridos")]
+
         public string N_Educativo { get; set; }
+    
     }
 }
